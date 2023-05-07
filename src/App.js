cdbdf27686931c10e9512ac019f1e34b2bb942aa
eldorado-22 <jumashevv992@gmail.com>
2023-05-08 00:44:54 +0600

    pages-popular

--- src/App.js
+++ src/App.js
@@ -4,16 +4,26 @@ import Header from "./components/Header/header";
 import Footer from "./components/Footer/footer";
 import {Route, Routes} from "react-router-dom";
 import Index from "./components";
+import Popular from "./pages/Popular/popular";
+import {useState} from "react";
 
 function App() {
+    const [dark, setDark] = useState(false)
+    function getDark() {
+        setDark(!dark)
+    }
     return (
-        <>
-            <Header/>
-            <Routes>
-                <Route path={"/"} element={<Index/>}/>
+        <div style={{
+            background: dark ? '#0f61b6' : 'black',
+            color: dark ? 'black' : 'blue'
+        }}>
+            <Header  getDark={getDark} dark={!dark}/>
+            <Routes getDark={getDark} dark={!dark}>
+                <Route path={"/"} element={<Index/>}  getDark={getDark} dark={dark}/>
+                <Route path={"/popular"} element={<Popular/>}  getDark={getDark} dark={dark}/>
             </Routes>
             <Footer/>
-        </>
+        </div>
     );
 }
 
---
+++ src/pages/Popular/popular.js
@@ -0,0 +1,61 @@
+import React, {useEffect, useState} from 'react';
+import "./popular.scss";
+import {ApiKey} from "../../ApiKey/ApiKey";
+import axios from "axios";
+
+// https://api.themoviedb.org/3/movie/popular?api_key=<<api_key>>&language=en-US&page=1
+const Popular = () => {
+    const [popular, setPopular] = useState([])
+    const getPopular = async () => {
+        const res = await axios(`https://api.themoviedb.org/3/movie/popular?api_key=${ApiKey}&language=en-US&page=1`)
+        const {data} = res
+        setPopular(data.results)
+    }
+    useEffect(() => {
+        getPopular()
+    })
+    console.log(popular)
+
+    const [scroll, setScroll] = useState(0)
+
+    const toScroll = () => {
+        setScroll(window.scrollY)
+    }
+    //
+    useEffect(() => {
+        window.addEventListener('scroll', toScroll)
+    }, [])
+
+
+    return (
+        <div id="popular" style={{
+            background: scroll > 50 ? '#b22222' : '',
+            backdropFilter: "blur(10px)",
+        }}>
+            <div className="container">
+                <h1 style={{
+                    color: scroll > 50  ? "#000" : ""
+                }}  className="text-center pt-7 font-medium text-3xl">Welcome To <span>POPULAR</span></h1>
+                <div className="popular  basis-1/5 flex-wrap">
+
+                    {
+                        popular.map(el => (
+                            <div className="flex flex-col basis-1/4 px-5 py-8">
+                                <img src={`https://www.themoviedb.org/t/p/w220_and_h330_face/${el.poster_path}`}
+                                     alt="img"/>
+                                <div className="popular--text">
+                                    <h1 style={{
+                                        color: scroll > 50  ? "#000" : ""
+                                    }} className="text-center py-2 font-semibold text-xl">{el.title}</h1>
+                                </div>
+                            </div>
+                        ))
+                    }
+
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Popular;
